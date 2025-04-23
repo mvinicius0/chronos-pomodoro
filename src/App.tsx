1258bf897a366dc58571fb5a22aa3d9ad3f4d3ed
Marcos Vinicius <marcosv.souza@telefonicati.onmicrosoft.com>
2025-04-22 23:02:11 -0300

    Add CountDown and DefaultInput components; enhance Menu and Logo styles; update global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -12,9 +15,32 @@ export function App() {
       </Container>
 
       <Container>
-        <section>
-          <p>Menu</p>
-        </section>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput id='task' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Nesse ciclo descanse por 5 min.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
---
+++ src/components/CountDown/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.css';
+
+export function CountDown() {
+  return <div className={styles.cantainer}>00:00</div>;
+}
