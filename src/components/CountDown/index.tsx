ad2e6826e84d1e25a0ef6fc950cbc14213b5063e
Marcos Vinicius <marcosv.souza@telefonicati.onmicrosoft.com>
2025-05-12 18:53:21 -0300

    Refactor TaskContext implementation by creating initialTaskState, updating TaskContextProvider, and enhancing context usage in App, CountDown, and MainForm components

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,8 +1,10 @@
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import styles from './styles.module.css';
-import { useTaskContext } from '../../contexts/TaskContext';
 
 export function CountDown() {
-  const taskContext = useTaskContext();
-  console.log(taskContext);
-  return <div className={styles.cantainer}>00:00</div>;
+  const { state } = useTaskContext();
+
+  return (
+    <div className={styles.cantainer}>{state.formattedSecondsRemaining}</div>
+  );
 }
