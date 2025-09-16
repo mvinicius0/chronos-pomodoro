92f14839ab80bfb7c1143edc7333d46319d33216
Vinicius <vinicius.msouza07@gmail.com>
2025-09-16 16:54:51 -0300

    feat: implement countdown timer and default input component with styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './styles/global.css';
 import { Container } from './components/ContainerComponent/index';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import DefaultInput from './components/DefaultInput';
 
 export function App() {
   return (
@@ -13,6 +15,36 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='Task'
+              type='text'
+              id='oi'
+              placeholder='digite a tarefa'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Iniciar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
---
+++ src/components/Cycles/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.css';
+
+export function Cycles() {
+  return <div className={styles.cycles}>0</div>;
+}
