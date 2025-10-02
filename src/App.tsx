50029c76f2ac16f308a1cc7f757c4805714fdf77
Vinicius <vinicius.msouza07@gmail.com>
2025-10-02 20:52:36 -0300

    feat: restructure App component and add new pages with layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,8 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/ContainerComponent/index';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import DefaultInput from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import DefaultButton from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+
+import { Home } from './pages/Home';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='Task'
-              type='text'
-              id='oi'
-              placeholder='digite a tarefa'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
