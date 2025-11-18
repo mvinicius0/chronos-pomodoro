0f0da23b89ea5533683ce99dfd80f28eff7705b3
Marcos Vinicius De Souza <marcos.vsouza+TEFBR@telefonicati.onmicrosoft.com>
2025-11-18 18:55:48 -0300

    feat: integrate TimerWorkerManager for task countdown and completion handling

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,7 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/taskContext/taskAction';
 import { Tips } from '../Tips';
+import { TimerWorkerManager } from '../../workers/timeWorkerManager';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -41,6 +42,12 @@ export function MainForm() {
     };
 
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+
+    const worker = TimerWorkerManager.getInstance();
+
+    worker.onmessage(event => {
+      console.log('Mensagem do worker:', event.data);
+    });
   }
 
   function handleInterruptActiveTask() {
--- src/contexts/taskContext/TaskContextProvider.tsx
+++ src/contexts/taskContext/TaskContextProvider.tsx
@@ -2,6 +2,8 @@ import { useEffect, useReducer } from 'react';
 import { TaskContext } from './TaskContext';
 import { initialTaskState } from './initialTaskState';
 import { taskReducer } from './taskReducer';
+import { TimerWorkerManager } from '../../workers/timeWorkerManager';
+import { TaskActionTypes } from './taskAction';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -10,9 +12,32 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage(e => {
+    console.log(e.data);
+    const countDownSeconds = e.data;
+
+    if (countDownSeconds <= 0) {
+      dispatch({ type: TaskActionTypes.COMPLETE_TASK });
+      worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
+    }
+  });
+
   useEffect(() => {
     console.log(state);
-  }, [state]);
+    if (!state.activeTask) {
+      console.log('no active task, terminating worker');
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [state, worker]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
