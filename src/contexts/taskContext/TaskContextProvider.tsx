9fd242f78fcd04bcd75e51bb73510d0c4fd955f1
Marcos Vinicius De Souza <marcos.vsouza+TEFBR@telefonicati.onmicrosoft.com>
2025-12-01 09:01:43 -0300

    feat: add web app manifest and favicon images
    - Created a web app manifest file for Chronos Pomodoro with relevant metadata.
    - Added 192x192 and 512x512 PNG icons for the web app.
    - Removed unused Vite and React SVG files from the assets.
    - Introduced audio files for notification sounds: beep, gravitational beep, and tic tac sounds.
    - Updated TaskContextProvider to play a beep sound when the timer completes.
    - Adjusted initial work time in the task context state for testing purposes.
    - Implemented a utility function to load and play the beep sound.

--- src/contexts/taskContext/TaskContextProvider.tsx
+++ src/contexts/taskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { TaskContext } from './TaskContext';
 import { initialTaskState } from './initialTaskState';
 import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/timeWorkerManager';
 import { TaskActionTypes } from './taskAction';
+import { loadBeep } from '../../utils/loadBeep';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -11,14 +12,18 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   const worker = TimerWorkerManager.getInstance();
 
   worker.onmessage(e => {
-    console.log(e.data);
     const countDownSeconds = e.data;
 
     if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
       dispatch({ type: TaskActionTypes.COMPLETE_TASK });
       worker.terminate();
     } else {
@@ -30,14 +35,20 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
   });
 
   useEffect(() => {
-    console.log(state);
     if (!state.activeTask) {
-      console.log('no active task, terminating worker');
       worker.terminate();
     }
 
     worker.postMessage(state);
   }, [state, worker]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
