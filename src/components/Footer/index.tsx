0170cc6e2f63b5e2f7e60fe3d68f3b4d301f923a
Marcos Vinicius De Souza <marcos.vsouza+TEFBR@telefonicati.onmicrosoft.com>
2026-01-05 10:45:40 -0300

    feat: replace Link with RouterLink component for consistent navigation

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,15 @@
-import { Link } from 'react-router';
 import styles from './styles.module.css';
+import { RouterLink } from '../RouterLink';
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link to='/about-pomodoro'>
+      <RouterLink href='/about-pomodoro'>
         Entenda como funciona a técnica pomodoro 🍅
-      </Link>
-      <Link to='/'>
+      </RouterLink>
+      <RouterLink href='/'>
         Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚
-      </Link>
+      </RouterLink>
     </footer>
   );
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,7 +7,7 @@ import {
 } from 'lucide-react';
 import styles from './styles.module.css';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router';
+import { RouterLink } from '../RouterLink';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -40,30 +40,30 @@ export function Menu() {
 
   return (
     <nav className={styles.menu}>
-      <Link
+      <RouterLink
         className={styles.menuLink}
-        to='/'
+        href='/'
         aria-label='Ir para a Home'
         title='Ir para a Home'
       >
         <HouseIcon />
-      </Link>
-      <a
+      </RouterLink>
+      <RouterLink
         className={styles.menuLink}
-        href='#'
+        href='/history/'
         aria-label='Ir para o Histórico'
         title='Ir para o Histórico'
       >
         <HistoryIcon />
-      </a>
-      <a
+      </RouterLink>
+      <RouterLink
         className={styles.menuLink}
-        href='#'
+        href='/settings/'
         aria-label='Ir para as Configurações'
         title='Ir para as Configurações'
       >
         <SettingsIcon />
-      </a>
+      </RouterLink>
       <a
         className={styles.menuLink}
         href='#'
